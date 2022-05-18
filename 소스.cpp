8c09621164f88329620388973f5e20b4bce6e585
Seonghun Jeong <eoeou1227@naver.com>
2022-05-18 19:26:48 +0900

    first commit

---
+++ 소스.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int test_one = 0;
+	int test_two = 0;
+	int total = 0;
+
+	test_one = 100;
+	test_two = 200;
+
+	total = test_one + test_two;
+
+	printf("total = %d", total);
+
+	return 0;
+}
